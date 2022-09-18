dc733b603647a8164e46f82420d2c7145242e301
cc <44751363+zzfn@users.noreply.github.com>
2022-09-18 11:20:05 +0800

    1

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 type Msg struct {
-	msg_type  string `json:"msg_Type"`
+	msg_type  string
 	timestamp int64
 	sign      string
 	content   Content
@@ -63,6 +63,64 @@ func GenSign(secret string, timestamp int64) (string, error) {
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
 	return signature, nil
 }
+func sendCard(apiUrl, secret string) {
+	contentType := "application/json"
+	currentTime := time.Now().Unix()
+	sign, _ := GenSign(secret, currentTime)
+	DroneBuildLink := os.Getenv("DRONE_BUILD_LINK")
+	DroneCommitLink := os.Getenv("DRONE_COMMIT_LINK")
+	sendData := `{
+	"timestamp": ` + strconv.FormatInt(currentTime, 10) + `,
+	"sign": "` + sign + `",
+	"msg_type": "interactive",
+    "card":{
+        "config":{
+            "wide_screen_mode":true
+        },
+        "elements":[
+            {
+                "tag":"hr"
+            },
+            {
+                "tag":"div",
+                "text":{
+                    "content":"🔺 [commit](` + DroneCommitLink + `)\n🔺 [drone](` + DroneBuildLink + `)",
+                    "tag":"lark_md"
+                }
+            }
+        ],
+        "header":{
+            "template":"blue",
+            "title":{
+                "content":"drone构建通知",
+                "tag":"plain_text"
+            }
+        }
+    }
+}`
+	fmt.Println(sendData)
+	resp, err := http.Post(apiUrl, contentType, strings.NewReader(sendData))
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+	if resp.StatusCode == http.StatusCreated {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		jsonStr := string(body)
+		fmt.Println("Response: ", jsonStr)
+	} else {
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println("Get failed with error: ", resp.Status, string(body))
+	}
+}
 func main() {
 	webhook := os.Getenv("PLUGIN_WEBHOOK")
 	secret := os.Getenv("PLUGIN_SECRET")
@@ -75,8 +133,9 @@ func main() {
 		fmt.Printf("请配置%s\n", "secret")
 		os.Exit(1)
 	}
-	if text == "" {
-		text = os.Getenv("DRONE_BUILD_LINK")
+	if text != "" {
+		sendMsg(webhook, text, secret)
+	} else {
+		sendCard(webhook, secret)
 	}
-	sendMsg(webhook, text, secret)
 }
